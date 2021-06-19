18a14b70376b58a678b9e0a65339504ada9aec84
beeth0ven <beeth0vendev@gmail.com>
2021-06-19 17:50:09 +0800

    add counter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,113 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:love/love.dart';
+import 'package:flutter_love/flutter_love.dart';
+import 'package:provider/provider.dart';
+
+// typedef CounterState = int;
+
+abstract class CounterEvent {}
+class CounterEventIncrease implements CounterEvent {}
+class CounterEventDecrease implements CounterEvent {}
+
+System<int, CounterEvent> createCouterSystem() {
+  return System<int, CounterEvent>
+    .create(initialState: 0)
+    .on<CounterEventIncrease>(
+      reduce: (state, event) => state + 1,
+      effect: (state, event, dispatch) async {
+        await Future.delayed(Duration(seconds: 3));
+        dispatch(CounterEventDecrease());
+      },
+    )
+    .on<CounterEventDecrease>(
+      reduce: (state, event) => state - 1,
+    )
+    .add(effect: (state, oldState, event, dispatch) {
+      print('\nEvent: $event');
+      print('State: $state');
+      print('OldState: $oldState');
+    })
+    .react<int>(
+      value: (state) => state,
+      skipFirstValue: true,
+      effect: (value, dispatch) {
+        print('Simulate persistence save call with state: $value');
+      },
+    );
+}
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UseReactWidgetPage(), // use `React` widget
+      // home: UseStoreProviderPage(), // or use `StoreProvider`
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+/// option 1: use `React` widget
+class UseReactWidgetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Provider(
+      create: (_) => createCouterSystem()
+        .share(),
+      builder: (context, _) => React.state<int, CounterEvent>(
+        context.read(), 
+        builder: (context, state, dispatch) {
+          return CounterPage(
+            title: 'Use React Widget Page',
+            count: state,
+            onIncreasePressed: () => dispatch(CounterEventIncrease()),
+          );
+        }
+      ),
+    );
+  }
+}
+
+/// option 2: use `StoreProvider`
+class UseStoreProviderPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider(
+      create: (_) => createCouterSystem()
+        .asEffectSystem(),
+      builder: (context, _) {
+        final store = context.watch<Store<int, CounterEvent>>();
+        return CounterPage(
+          title: 'Use Store Provider Page', 
+          count: store.state, 
+          onIncreasePressed: () => store.dispatch(CounterEventIncrease())
+        );
+      },
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+
+  const CounterPage({
+    Key? key,
+    required this.title,
+    required this.count,
+    required this.onIncreasePressed,
+  }) : super(key: key);
+
+  final String title;
+  final int count;
+  final VoidCallback onIncreasePressed;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
+        title: Text(title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$count',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: onIncreasePressed,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }

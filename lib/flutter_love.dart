621baa384adb0338bd117f63763a84be645e5c0a
beeth0ven <beeth0vendev@gmail.com>
2021-11-22 10:17:11 +0800

    remove dependency `provider` form `flutter_love`

--- lib/flutter_love.dart
+++ lib/flutter_love.dart
@@ -1,10 +1,8 @@
 library flutter_love;
 
 export 'package:love/love.dart';
-export 'src/dispatch_x.dart' show DispatchX;
 export 'src/react.dart' 
   show 
     React,
     ReactState,
     WidgetBuilder; 
-export 'src/system_providers.dart' show SystemProviders;
